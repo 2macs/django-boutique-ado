735debfe4688ca8394a795d982affb3a021bb89d
2macs <108128171+2macs@users.noreply.github.com>
2023-05-23 19:41:58 +0100

    Delete env.py

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ['SECRET_KEY']= 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
-os.environ['STRIPE_PUBLIC_KEY'] = 'pk_test_51Mzh6nK0F9yAasNPCBArpbuga6LTECX18E4nSeOeB6WWNJhzs8LMIO3aGxh05LKAvgMMEGtdLpaQAmkahrcVjTWk00Q0eanRkr'
-os.environ['STRIPE_SECRET_KEY'] = 'sk_test_51Mzh6nK0F9yAasNP481YBBKypenfQmWg0QvKmAYCzbfFBkPAr9S0fty4E2VAeadPjsN0RYAZi6DmZGa5pctOUgGo00Qd6CRkqA'
-os.environ['AWS_ACCESS_KEY_ID'] = 'AKIAX53HXSF4JZPOOKAH'
-os.environ['AWS_SECRET_ACCESS_KEY'] = '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'
