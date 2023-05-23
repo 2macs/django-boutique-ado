39f3019993cde2dafb0d83c11201441c8bf9b7c5
2macs <adrian.mcneill@outlook.com>
2023-05-23 15:47:47 +0000

    final files

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import Product, Category
 
-
 # Register your models here.
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'sku',
@@ -12,15 +12,14 @@ class ProductAdmin(admin.ModelAdmin):
         'rating',
         'image',
     )
-    ordering = ('sku', )
 
+    ordering = ('sku',)
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
         'friendly_name',
         'name',
     )
-
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Category, CategoryAdmin)
